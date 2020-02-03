56dbb09e9e0192ef4a92f42fc5b9f4cf22650dc5
Patrascu Tiberiu <43850001+idle91@users.noreply.github.com>
2020-02-03 16:56:56 +0100

    03/02/2020v2

--- flipquizz/WebApp/Debug.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function d($var = null)
-{
-    echo '<pre>';
-    echo '<h3>'.gettype($var).'</h3>';
-    echo var_export($var,true);
-    echo '</pre>';
-}
--- flipquizz/WebApp/www/js/Team.js
+++
@@ -1,9 +0,0 @@
-class Team
-{
-    constructor(_id) {
-        this.id = parseInt(_id);
-        this.name = 'Team #' + this.id;
-        this.avatar = '';
-        this.score = 0;
-    }
-}
